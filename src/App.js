ff3d6cafada28f7d9c9694dc8766e1e8818be53c
Rafhy Khan <rafhy.mk@hotmail.com>
2024-05-12 12:44:36 -0400

    pdate

--- src/App.js
+++ src/App.js
@@ -4,12 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           What kind of code you got man, whats going on?
         </p>
-      </header>
     </div>
   );
 }
