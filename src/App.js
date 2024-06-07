e1e52f70fd2d11d0ad6885871561c3943b5b5005
Rafhy Khan <rafhy.mk@hotmail.com>
2024-06-07 11:49:43 -0600

    Hello WOrld

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
         <div style={{border: "1px solid black"}}>
           card info
         </div>
+        <div>
+          <p> The reason: Back before the age of time and man there was a goo, the goo was boo the boo was scary ad the scry was a ghost called neko. Ooo</p>
+        </div>
     </div>
   );
 }
