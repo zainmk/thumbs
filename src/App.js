582a3907750d17899cb3fe5035104cf675fafec8
Rafhy Khan <rafhy.mk@hotmail.com>
2024-06-07 11:56:15 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 
 import './App.css';
 
+
+
+//Make a div, than do what you want in your card.
+
 function App() {
   return (
     <div className="App">
         <div style={{border: "1px solid black"}}>
           card info
+          <p>------------------------------------- </p>
         </div>
         <div>
           <p> The reason: Back before the age of time and man there was a goo, the goo was boo the boo was scary ad the scry was a ghost called neko. Ooo</p>
         </div>
+
+
+       <div>
+        <p> What up neko?</p>
+       </div>
+
+
+       
     </div>
   );
 }
