88e9b9280fd071dfadc502928ad66a2cd37fbf91
zainmk <zain.khan@heliolytics.com>
2024-06-09 17:11:56 -0400

    moving to helper

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,10 +1,12 @@
 //Function: For Returning Star Wars API Cal
-export function httpGet(theUrl) {
+export function httpGet(theUrl, headerText="'Content-type', 'application/x-www-form-urlencoded'")
+{
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.setRequestHeader(headerText.substring(1, headerText.search(",")-1), headerText.substring(headerText.search(",")+3, headerText.length-1));
     xmlHttp.send( null );
     //Returns xmlHttp.responseText containing whole object in string form, only want name part
-    const name = xmlHttp.responseText.substring(xmlHttp.responseText.search("name")+6 , xmlHttp.responseText.search("height")-2)
+    
 
-    return name;
+    return xmlHttp.responseText;
 }
