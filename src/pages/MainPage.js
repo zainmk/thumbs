c628d1a31b9e610d7289ea01b1116a73c9db1887
zainmk <zain.khan@heliolytics.com>
2024-06-12 19:34:03 -0400

    removing default

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -8,36 +8,13 @@ import CardList from '../components/CardList.js';
 
 //Make a div, than do what you want in your card.
 
-const testData = [
-  {
-    Type: "movie",
-    Title: "Star Wars: Episode IV - A New Hope",
-    Year: 1977,
-    imdbID: "tt0076759",
-    Poster: ""
-  },
-  {
-    Type: "movie",
-    Title: "Inception",
-    Year: 2010,
-    imdbID: "tt1375666",
-    Poster: ""
-  },
-  {
-    Type: "game",
-    Title: "Elden Ring",
-    Year: 2022,
-    imdbID: "tt10562854",
-    Poster: ""
-  }
-]
 
 function MainPage() {
 
-  const [cardData, setCardData] = useState(testData) // TODO: consider a context for this variable // Add this to cache/cookies (keep track of entries locally
+  const [cardData, setCardData] = useState([]) // TODO: consider a context for this variable // Add this to cache/cookies (keep track of entries locally
 
   useEffect(() => {
-    if(cardData !== testData){
+    if(cardData.length !== 0){
       localStorage.setItem('cardData', JSON.stringify(cardData));
     }
   }, [cardData]);
@@ -45,7 +22,7 @@ function MainPage() {
 
   useEffect(() => {
     const existingCardData = JSON.parse(localStorage.getItem('cardData'));
-    setCardData(existingCardData ? existingCardData : testData)
+    setCardData(existingCardData ? existingCardData : [])
   }, []);
 
   return (
