6a8155a4eddb6337213e6ce29fe78721c5b74856
Rafhy Khan <rafhy.mk@hotmail.com>
2024-06-21 18:04:53 -0600

    Big text changes

--- src/components/TextCard.js
+++ src/components/TextCard.js
@@ -1,10 +1,21 @@
 import * as React from 'react';
+import MovingComponent from "react-moving-text";
 
 function TextCard(){
 
     return (
         <>
-            <p> Welcome to thumbs! Search for the movies you enjoyed and create your favourites list! </p>
+        <br/>
+            <MovingComponent
+                type="bounce"
+                duration="1000ms"
+                delay="0s"
+                direction="normal"
+                timing="ease"
+                iteration="5"
+                fillMode="none">
+                Welcome to thumbs! Search for the movies you enjoyed and create your favourites list! 
+            </MovingComponent>
         </>
     )
 }
