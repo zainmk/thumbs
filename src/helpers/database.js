9ec51842ea28e68805def65b8bda44cf78b859c8
Rafhy K <Rafhy.mk@hotmail.com>
2024-06-22 22:17:05 -0600

    Update database.js, committing potential updates to do

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -7,3 +7,17 @@ export const createUser = async({ username, password }) => fetch(FIREBASE_USERS(
 export const getUser = async(user) => fetch(FIREBASE_USERS(user)).then(res => res.json())
 export const getMediaList = async(user) => fetch(FIREBASE_USERS(`${user}/media_list`)).then(res => res.json())
 export const updateMediaList = async(user, newMediaList) => fetch(FIREBASE_USERS(`${user}/media_list`), { method: 'PUT', body: JSON.stringify(newMediaList)})
+
+
+/* 
+Make a function that, increments the total number of likes for a 
+specific movie. Global among users. 
+
+Make a function that, returns the number of likes of a specific
+movie. Global mong users 
+
+Make a function that sends to databas, user liked that 
+specific movie or not, can be boolean value. 
+Each movie can have a "Liked" property, true, false, null?
+
+*/
