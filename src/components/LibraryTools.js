48729dcc04b74d646d19ac45a99f4d9de6f00f2f
Zain Khan <zain.mk@hotmail.com>
2024-08-21 22:10:45 -0600

    library tools excusive buttons

--- src/components/LibraryTools.js
+++ src/components/LibraryTools.js
@@ -3,28 +3,26 @@ import { IconButton } from '@mui/material';
 import PreviewIcon from '@mui/icons-material/Preview';
 import RecommendIcon from '@mui/icons-material/Recommend';
 
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+
 
 function LibraryTools({ status, setStatus }){
 
-    const toDislikeStyles = { border: status==="dislike" ? "1px solid white" : "", margin: status==="dislike" ? "-1px" : "" }
-    const toLikeStyles = { border: status==="like" ? "1px solid white" : "", margin: status==="like" ? "-1px" : "" }
-    const toWatchStyles = { border: status==="watch" ? "1px solid white" : "", margin: status==="watch" ? "-1px" : "" }
-
     return (
         <>
-            <IconButton onClick={()=> setStatus(status === 'dislike' ? '':'dislike')} sx={{ transform: "rotate(180deg)" }} > 
-                <RecommendIcon sx={toDislikeStyles} /> 
-            </IconButton>
-
-            <IconButton onClick={()=> setStatus(status === 'like' ? '' : 'like')}> 
-                <RecommendIcon sx={toLikeStyles} /> 
-            </IconButton>
-
-            <IconButton onClick={()=> setStatus(status === 'watch' ? '' : 'watch')}>
-                <PreviewIcon sx={toWatchStyles} /> 
-            </IconButton>
+            <ToggleButtonGroup sx={{ marginTop: "10px" }} size="small" value={status} exclusive onChange={(event, newStatus) => setStatus(newStatus === status ? '' : newStatus)}>
+                <ToggleButton value={"dislike"} sx={{ transform: "rotate(180deg)" }}>
+                    <RecommendIcon />
+                </ToggleButton>
+                <ToggleButton value={"like"}>
+                    <RecommendIcon />
+                </ToggleButton>
+                <ToggleButton value={"watch"}>
+                    <PreviewIcon />
+                </ToggleButton>
+            </ToggleButtonGroup>
         </>
-        
     )
 
 }
